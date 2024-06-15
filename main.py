f18050958d9ac90552751ac61114747d3e18f5e2
Mushtaq Ali <49233294+ali1810@users.noreply.github.com>
2024-06-16 00:05:04 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,49 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+import contact,home,project
+
+st.set_page_config(page_title="Solubility Prediction")
+
+
+
+class MultiApp:
+
+    def __init__(self):
+        self.apps = []
+
+    def add_app(self, title, func):
+
+        self.apps.append({
+            "title": title,
+            "function": func
+        })
+
+    def run():
+        # app = st.sidebar(
+        with st.sidebar:        
+            app = option_menu(
+                menu_title='fgdf Prediction ',
+                options=['Home','Project','Contact'],
+                icons=['house-fill','person-circle','telephone'],
+                menu_icon='chat-text-fill',
+                default_index=1,
+                styles={
+                  #  "container": {"padding": "5!important","background-color":'white'},
+        "icon": {"color": "black", "font-size": "20px"}, 
+        "nav-link": {"color":"black","font-size": "15px", "text-align": "left", "margin":"1px", "--hover-color": "blue"},
+        "nav-link-selected": {"background-color": "#02ab21"},}
+                
+                )
+
+        
+        if app == "Home":
+             home.app()
+        if app == "Project":
+             project.app()    
+        if app == "Contact":
+             contact.app()        
+      
+             
+    run()            
+         
